0647624f850c079e1920b1b5031862eea43575cf
SHIMIZU Taku <shimizu.taku@gmail.com>
2017-08-04 21:44:44 +0900

    Add sample Item

--- anondrapy/items.py
+++ anondrapy/items.py
@@ -8,7 +8,9 @@
 import scrapy
 
 
-class AnondrapyItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ArticleByDate(scrapy.Item):
+    """
+    日付ページを起点とした記事を表すItem
+    """
+    title = scrapy.Field()
+    body = scrapy.Field()
